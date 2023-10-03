85aab572fdde0dd46dcee3855d6b98dcb531b275
Deepraj <deepdev.js@gmail.com>
2023-10-03 12:56:01 +0530

    Fixed padding issue in header form

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
-        <form className="bg-slate-100 p3 rounded-lg flex items-center">
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
             type="text"
             placeholder="Search..."
